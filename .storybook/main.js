c06d2e8d02607a7e9dcbf0531ee9a32a5c7e0b72
Gean Gomes <geansg4@gmail.com>
2021-01-23 17:57:38 -0300

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.mdx",
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-knobs",
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
+    "@storybook/addon-essentials"
+  ]
+}
